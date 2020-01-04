10eed78d2963de0104ed59ff877ca7b8b8bea4a4
Nathan Hynes <nathanmail@hotmail.co.uk>
2020-01-04 17:04:58 +0000

    third formatter test passing

--- __tests__/formatter.test.js
+++ __tests__/formatter.test.js
@@ -7,9 +7,16 @@ describe('Formatter', () => {
       expect(Formatter.formatInput(data)).toEqual(["5 4"]);
     });
 
-    test("takes input string of multiple data and formats to array", () => {
-      const data = "5 4\n1 2\nNNESEESWNWW"
+    test("takes input string with linebreaks and formats to array", () => {
+      const data = "5 4\n1 2\nNNESEESWNWW";
       expect(Formatter.formatInput(data)).toEqual(["5 4", "1 2", "NNESEESWNWW"]);
+    });
+  });
+
+  describe('#convertStrToCoord', () => {
+    test("converts dimension string into integer array", () => {
+      const dimension = "5 4";
+      expect(Formatter.convertStrToCoord(dimension)).toEqual([5,4]);
     });
   });
 });
--- src/formatter.js
+++ src/formatter.js
@@ -1,7 +1,14 @@
-'use strict';
+
 
 function formatInput(inputData) {
   return inputData.split('\n');
 }
 
+function convertStrToCoord(coordString) {
+  return coordString.split(' ').map(
+    coord => parseInt(coord, 10)
+  );
+}
+
 module.exports.formatInput = formatInput;
+module.exports.convertStrToCoord = convertStrToCoord;
